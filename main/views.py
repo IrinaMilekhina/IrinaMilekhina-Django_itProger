afba54f914d1f0769adabd74618f9b628f1a83e8
IrinaMilekhina <i.mezhova@gmail.com>
2021-07-27 17:12:03 +0300

    добавление контекста в шаблоны, добавление фильтра upper

--- main/views.py
+++ main/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    context = {'title': 'Главная страница'}
+    return render(request, 'main/index.html', context)
 
 def about(request):
-    return render(request, 'main/about.html')
+    context = {'title': 'Про нас'}
+    return render(request, 'main/about.html', context)
